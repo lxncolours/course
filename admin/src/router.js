3bd9ecfdb1e7a1e3e378c975baea038ac568f3d8
liberica <363440207@qq.com>
2021-06-25 14:40:32 +0800

    1.修复右上角弹窗不生效问题 2.简洁左方导航栏内容 3.新增admin欢迎页welcome

--- admin/src/router.js
+++ admin/src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
 
 Vue.use(Router);
 
@@ -20,7 +21,11 @@ export default new Router({
   },
     {
       path: "/admin",
-      component: Admin
+      component: Admin,
+      children: [{
+        path: "welcome",
+        component: Welcome,
+      }]
     }
   ]
 })
---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1> 欢迎进入造梦后台管理系统</h1>
+  </div>
+</template>
