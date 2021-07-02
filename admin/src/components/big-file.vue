84cd0206defc4557c416d55e40b5b75e1f3e281f
liberica <363440207@qq.com>
2021-07-02 19:21:53 +0800

    1.使用阿里云oss进行文件管理 2.将文件上传到阿里云oss服务器

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -78,7 +78,7 @@ export default {
       }
 
       // 文件分片
-      let shardSize = 10 * 1024 * 1024;    //以10MB为一个分片
+      let shardSize = 1 * 1024 * 1024;    //以1MB为一个分片
       let shardIndex = 1;		//分片索引，1表示第1个分片
       let size = file.size;
       let shardTotal = Math.ceil(size / shardSize); //总片数
@@ -147,7 +147,7 @@ export default {
 
         param.shard = base64;
 
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', param).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/oss-append', param).then((response) => {
           let resp = response.data;
           console.log("上传文件成功：", resp);
           Progress.show(parseInt(shardIndex * 100 / shardTotal));
