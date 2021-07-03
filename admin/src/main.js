542f39a1c0061d6a823cdf92df5248c2f3c52460
liberica <363440207@qq.com>
2021-07-03 15:47:10 +0800

    1.前后端增加登录拦截

--- admin/src/main.js
+++ admin/src/main.js
@@ -10,11 +10,33 @@ Vue.prototype.$ajax = axios;
 // 解决每次ajax请求，对应的sessionId不一致的问题
 axios.defaults.withCredentials = true;
 
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+    // 要不要对meta.loginRequire属性做监控拦截
+    if (to.matched.some(function (item) {
+        return item.meta.loginRequire
+    })) {
+        let loginUser = Tool.getLoginUser();
+        if (Tool.isEmpty(loginUser)) {
+            next('/login');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
 /**
  * axios拦截器
  */
 axios.interceptors.request.use(function (config) {
     console.log("请求：", config);
+    let token = Tool.getLoginUser().token;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token;
+        console.log("请求headers增加token:", token);
+    }
     return config;
 }, error => {
 });
@@ -33,5 +55,6 @@ new Vue({
     router,
     render: h => h(App),
 }).$mount('#app');
+
 
 console.log("环境：", process.env.NODE_ENV);
